d3e77a38c888c866135134e7fa1415f52d5b372d
unknown <cspen@modintro.com>
2019-09-18 23:32:08 -0500

    Implement process_request function

--- system/controller/SOSController.php
+++ system/controller/SOSController.php
@@ -12,6 +12,43 @@ class SOSController implements Settings {
 		
 	}
 	
+	// Called during program execution
+	public function process_request() {
+		$path = str_replace(Settings::APP_URL, "", $_SERVER['REQUEST_URI']);
+		$tail = substr($_SERVER['REQUEST_URI'], -1);
+		
+		// Need to change the logic flow from
+		// Controller->Model->View to
+		// Controller->Model->Controller->View
+		
+		// Set the view
+		if(empty($path)) {
+			$this->view->setTemplate(SOSView::HOME);
+			$this->model->update_state($path, null);
+		} else if($tail === "/") {
+			
+			
+			
+			if($_SERVER['REQUEST_METHOD'] === 'PUT') {
+				$this->view->headerOnly('405 Method Not Allowed');
+			}
+			$this->view->setTemplate(SOSView::TOPIC);
+			$this->model->update_state($path, SOSModel::TOPIC);
+		} else {
+			if(strcmp($path, Settings::ADMIN_LOGIN_PAGE) == 0) {
+				$this->token();
+				$this->model->login($_SESSION['token']);
+				$this->view->setTemplate(SOSView::ADMIN_LOGIN);
+			} else if(strcmp($path, Settings::SYS_OPS) == 0) {
+				$this->system_operation($tail);
+				exit;
+			} else {
+				$this->view->setTemplate(SOSView::ARTICLE);
+				$this->model->update_state($path, SOSModel::ARTICLE);
+			}
+		}
+	}
+	
 	private $model;
 	private $view;	
 }
