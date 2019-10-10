f2195f7a0becd6b8a939c70d5fa5c494cbe40ae4
unknown <cspen@modintro.com>
2019-10-09 23:56:57 -0500

    Implement constructor

--- system/view/SOSView.php
+++ system/view/SOSView.php
@@ -3,10 +3,10 @@
 class SOSView implements Settings {
 	
 	public function __construct() {
-		
+		$this->template = $this::HOME; // Default
 	}
 	
-	
+	private $template;		// The template for the page
 }
 
 ?>
