fb85c27936efa46c119ea499fe431a4fc9838563
unknown <cspen@modintro.com>
2019-10-14 23:37:43 -0500

    Imlement setters

--- system/view/SOSView.php
+++ system/view/SOSView.php
@@ -14,26 +14,30 @@ class SOSView implements Settings {
 	}	
 	
 	public function setPageTemplate($template) {
-		
+		$this->template = $template;
 	}
 	
 	public function setPageContent($content) {
-		
+		$this->content = $content;
 	}
 	
 	public function setPageNavigationMenu($menu) {
-		
+		$this->menu = $menu;
 	}
 	
 	public function setPageFeature($feature) {
-		
+		$this->feature = $feature;
 	}
 	
 	public function setPageFooter($footer) {
-		
+		$this->footer = $footer;
 	}
 		
 	private $template;		// The template for the page
+	private $content;		// Page content
+	private $menu;			// Site menu
+	private $feature;		// Page feature
+	private $footer;		// Page footer
 	
 	const HOME = "Template/home_template.php";
 }
