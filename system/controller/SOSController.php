80f088e181cc6bd2df4f2f52b5d81962498d64f8
unknown <cspen@modintro.com>
2019-10-31 23:54:46 -0500

    Debug logic flow

--- system/controller/SOSController.php
+++ system/controller/SOSController.php
@@ -8,30 +8,29 @@ class SOSController implements Settings {
 		$this->view = $view;
 	}
 	
-	public function test() {
-		
-	}
-	
-	// Called during program execution
 	public function process_request() {
 		$path = str_replace(Settings::APP_URL, "", $_SERVER['REQUEST_URI']);
 		$tail = substr($_SERVER['REQUEST_URI'], -1);
 		
-		if(empty($path)) { // Home Page			
+		
+		echo '<br>'.$path.'<br>';
+		if(empty($path)) { // Home Page	
+			echo '<br>'.$path.'<br>';
 			try {
 				$this->model->home();
 				$this->view->setTemplate(SOSView::HOME);
 			} catch(Exception $e) {
 				// Need to develop error handling
 			}
-		} else if($tail === "/") { // Category - List of Articles			
+		} else if($tail === "/") { // Category - List of Articles	
+			echo '<br>Controller 2<br>';
 			if($_SERVER['REQUEST_METHOD'] === 'PUT') {
 				$this->view->headerOnly('405 Method Not Allowed');
 			}		
 			
 			try {
-				$this->model->topic($path);
-				$this->view->setTemplate(SOSView::TOPIC);
+				$this->model->category($path);
+				// $this->view->setTemplate(SOSView::TOPIC);
 			} catch(Exception $e) {
 				// Error
 			}			
@@ -46,6 +45,7 @@ class SOSController implements Settings {
 				$this->system_operation($tail);
 				exit;
 			} else {
+				// Show the article
 				try {
 					$this->model->article($path);
 					$this->view->setTemplate(SOSView::ARTICLE);					
@@ -55,12 +55,33 @@ class SOSController implements Settings {
 			}
 		}
 	}
-	
+		
 	/**
-	 * 
+	 * Set session token for form validation
+	 * to prevent CSFR attack.
 	 */
 	private function token() {
-		
+		if (session_status() == PHP_SESSION_NONE) {
+			session_start();
+		}
+		if (empty($_SESSION['token'])) { 
+			$_SESSION['token'] = bin2hex(random_bytes(32));
+		}
+	}
+	
+	/**
+	 * Prevent CSRF attack by verifying the token
+	 * sent with the form. The token is created
+	 * when the form is created and the token value
+	 * is stored in the session variable.
+	 */
+	private function verify_token() {
+		if (!empty($_POST['token']) && !empty($_SESSION['token'])) {
+			if (hash_equals($_SESSION['token'], $_POST['token'])) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	private $model;
