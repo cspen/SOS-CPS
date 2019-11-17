f69dcde9d2b17d1ea664e34d437020a5dffd6b30
unknown <cspen@modintro.com>
2019-11-17 00:19:06 -0600

    Add getters

--- system/model/Article.php
+++ system/model/Article.php
@@ -8,7 +8,45 @@ class Article {
 	
 // Getters
 
-	// public function 
+	public function getTitle() {
+		return $this->title;
+	}
+	
+	public function getDescription() {
+		return $this->description;
+	}
+	
+	public function getBody() {
+		return $this->body;
+	}
+	
+	public function getPublishStatus() {
+		return $this->publishStatus;
+	}
+	
+	public function getPublishDate() {
+		return $this->publishDate;
+	}
+	
+	public function getCreationDate() {
+		return $this->creationDate;
+	}
+	
+	public function getPath() {
+		return $this->path;
+	}
+	
+	public function getParent() {
+		return $this->parent;
+	}
+	
+	public function getEtag() {
+		return $this->etag;
+	}
+	
+	public function getLastModified() {
+		return $this->lastModified;
+	}
 
 // Setters
 	
